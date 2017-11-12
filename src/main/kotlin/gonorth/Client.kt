6365549d614b22de62fbdb5da56b43fc270a4d55
rawtoast <jim@jamesmorris.org>
2017-11-12 20:15:47 +0000

    Larger world

--- src/main/kotlin/gonorth/Client.kt
+++ src/main/kotlin/gonorth/Client.kt
@@ -106,14 +106,33 @@ object BasicWorld {
         val p1 = Location(UUID.randomUUID(), "There is a fork in the path.")
         val p2 = Location(UUID.randomUUID(), "The path comes to an abrupt end.")
         val p3 = Location(UUID.randomUUID(), "You went north and died.")
+        val p4 = Location(UUID.randomUUID(), "The road continues to the east, whilst a side path heads south.")
+        val p5 = Location(UUID.randomUUID(), "A river blocks your path.")
+        val p6 = Location(UUID.randomUUID(), "To the north you spot a large tower.")
+        val p7 = Location(UUID.randomUUID(), "You look at the tower door in front of you. Rocks fall, You die.")
+
+
 
         val world = WorldBuilder().newLocation(p1)
                 .newLocation(p2)
                 .newLocation(p3)
+                .newLocation(p4)
+                .newLocation(p5)
+                .newLocation(p6)
                 .twoWayLink(p1, p2, Move.EAST, Move.WEST,
                         "You take the path heading east",
                         "You make your way back to the crossroads")
+                .twoWayLink(p1, p4, Move.WEST, Move.EAST,
+                        "You take the path heading west",
+                        "You make your way back to the crossroads")
+                .twoWayLink(p4, p5, Move.SOUTH, Move.NORTH,
+                        "You take the beaten path south",
+                        "You take the worn path north")
+                .twoWayLink(p4, p6, Move.EAST, Move.WEST,
+                        "You take the trail to the east",
+                        "As you head west, you ponder whether println can print strings")
                 .linkLocation(p1, p3, Move.NORTH, "You stumble ahead")
+                .linkLocation(p6, p7, Move.NORTH, "You head north towards the tower")
                 .world
 
 
