e593efcab8ebd711b4f4e799532b0747c8f70544
RawToast <jim@jamesmorris.org>
2018-05-12 14:53:06 +0100

    Wrap Terminal Reader in Try (#16)

--- src/main/kotlin/gonorth/console/Console.kt
+++ src/main/kotlin/gonorth/console/Console.kt
@@ -4,18 +4,18 @@ import arrow.core.Try
 import arrow.core.getOrElse
 import org.jline.terminal.Terminal
 
-class GnConsole(private val terminal: Terminal): Console {
+class GnConsole(private val terminal: Terminal) : Console {
     override fun output(text: String) {
         println(text + "\r")
     }
 
     override fun awaitInput(): Char =
-        Try.just(terminal
-                .reader()
-                .read()
-                .toChar()
-                .toLowerCase())
-                    .getOrElse { ' ' }
+        Try {
+            terminal.reader()
+                    .read()
+                    .toChar()
+                    .toLowerCase()
+        }.getOrElse { ' ' }
 
     override fun clear() {
         print("\u001B[H\u001B[2J")
